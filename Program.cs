41e664ee55170873f6265899ca4758b66b287eeb
Alex-cadet <63915117+Alex-cadet@users.noreply.github.com>
2020-04-18 23:05:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rew = " ";
+            string res = " ";
+            Random rnd = new Random();
+            int[] Arr = new int[10];
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Arr[i] = rnd.Next(30);
+                rew = rew + Arr[i].ToString() + " ";
+            }
+            Console.WriteLine("Заданная строка: {0}", rew);
+            for (int j = 0; j < Arr.Length; j++)
+                if (Arr[j] % 2 == 0)
+                    res = res + Arr[j].ToString() + " ";
+            Console.WriteLine("Чётные символы: {0}", res);
+        }
+    }
+}
